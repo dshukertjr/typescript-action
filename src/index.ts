f886d70bd68b6086835031cb280e8ee377f74052
Hub Tyler <18113850+dshukertjr@users.noreply.github.com>
2021-06-15 13:55:23 +0900

    Changed some setting

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,25 @@
-import * as hubspot from '@hubspot/api-client' 
+import * as hubspot from '@hubspot/api-client'
 
-exports.main = (event, callback) => {
+exports.main = async (event, callback) => {
   const hubspotClient = new hubspot.Client({
     apiKey: process.env.HAPIKEY
-  });
+  })
   hubspotClient.crm.contacts.basicApi.getById(event.object.objectId, ["email", "phone"])
     .then(results => {
-      let email = results.body.properties.email;
-      let phone = results.body.properties.phone;
+      let email = results.body.properties.email
+      let phone = results.body.properties.phone
+
+      console.log('email', email)
+
       callback({
         outputFields: {
           email: email,
           phone: phone
         }
-      });
+      })
     })
     .catch(err => {
-      console.error(err);
-    });
+      console.error(err)
+    })
 }
 
