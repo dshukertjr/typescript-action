6df9c6afe902e267f3d5a31db8621f3b2f8d0133
Hub Tyler <18113850+dshukertjr@users.noreply.github.com>
2021-06-15 11:40:52 +0900

    Added all of the packages

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import * as hubspot from '@hubspot/api-client' 
+
+exports.main = (event, callback) => {
+  const hubspotClient = new hubspot.Client({
+    apiKey: process.env.HAPIKEY
+  });
+  hubspotClient.crm.contacts.basicApi.getById(event.object.objectId, ["email", "phone"])
+    .then(results => {
+      let email = results.body.properties.email;
+      let phone = results.body.properties.phone;
+      callback({
+        outputFields: {
+          email: email,
+          phone: phone
+        }
+      });
+    })
+    .catch(err => {
+      console.error(err);
+    });
+}
+
